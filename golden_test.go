f49d06d9f49e9a8c9f0833e6be2fdd9f0167d172
Michael Stapelberg <michael@stapelberg.de>
2018-11-05 22:15:45 +0100

    tests: update to i3 v4.16

--- golden_test.go
+++ golden_test.go
@@ -37,7 +37,7 @@ func TestGoldensSubprocess(t *testing.T) {
 		}
 		want := Version{
 			Major:                4,
-			Minor:                15,
+			Minor:                16,
 			Patch:                0,
 			LoadedConfigFileName: abs,
 		}
