cf51dd9dba483c074bc7c055fc36fe504d24d5ef
Ivan Milov <522518+ivanmilov@users.noreply.github.com>
2021-04-03 11:35:41 +0200

    remove FindFocusedLeaf method

--- tree_utils.go
+++ tree_utils.go
@@ -2,11 +2,8 @@ package i3
 
 import "strings"
 
-func (n *Node) FindFocusedLeaf() *Node {
-	return n.FindFocused(func(n *Node) bool { return n.Focused })
-}
-
-// FindParent method returns the parent node of the curren one
+// FindParent method returns the parent node of the current one
+// by requesting and traversing the tree
 func (child *Node) FindParent() *Node {
 	tree, err := GetTree()
 	if err != nil {
